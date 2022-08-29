7a44f6975e0f8ea18b42e47c55f10b8b7e432c68
Steven Hastings <steve.hastings0321@gmail.com>
2022-08-29 01:23:59 -0400

    updated requirements.txt

--- app/apprequel.py
+++ app/apprequel.py
@@ -1,20 +1,54 @@
 import streamlit as st
 import pandas as pd
-
-#DB MGMT
+# DB MGMT
 import sqlite3
-conn = sqlite3.connect("data_bank\sqlplayground_app\sqlplayground_app\data\world.sqlite")
+
+conn = sqlite3.connect("C:\\Users\\steve\\PycharmProjects\\sql_playground\\data\\world.sqlite")
 c = conn.cursor()
 
-#FUNCTION MAKE EXECUTION
+conn = sqlite3.connect("C:\\Users\\steve\\PycharmProjects\\sql_playground\\data\\chinook.db")
+c = conn.cursor()
+
+
+# FUNCTION MAKE EXECUTION
 def sql_executor(raw_code):
     c.execute(raw_code)
     data = c.fetchall()
-    return data 
+    return data
+
 
 city = ['ID,', 'Name,', 'CountryCode,', 'District,', 'Population']
-country = ['Code,', 'Name,', 'Continent,', 'Region,', 'SurfaceArea,', 'IndepYear,', 'Population,', 'LifeExpectancy,', 'GNP,', 'GNPOld,', 'LocalName,', 'GovernmentForm,', 'HeadOfState,', 'Capital,', 'Code2']
+
+country = ['Code,', 'Name,', 'Continent,', 'Region,', 'SurfaceArea,', 'IndepYear,', 'Population,', 'LifeExpectancy,',
+           'GNP,', 'GNPOld,', 'LocalName,', 'GovernmentForm,', 'HeadOfState,', 'Capital,', 'Code2']
+
 countrylanguage = ['CountryCode,', 'Language,', 'IsOfficial,', 'Percentage']
+
+employees = ['employee_id', 'first_name', 'last_name',
+             'email', 'phone', 'address', 'city', 'state',
+             'country', 'postal_code', 'active', 'hire_date', 'termination_date']
+
+customers = ['customer_id', 'first_name', 'last_name', 'email',
+             'phone', 'address', 'city', 'state', 'country',
+             'postal_code', 'active', 'credit_limit']
+
+invoices = ['invoice_id', 'customer_id', 'invoice_date',
+            'billing_address', 'billing_city', 'billing_state',
+            'billing_country', 'billing_postal_code', 'total']
+
+invoice_items = ['invoice_item_id', 'invoice_id', 'track_id',
+                 'unit_price', 'quantity', 'discount']
+
+tracks = ['track_id', 'name', 'album_id', 'media_type_id', 'genre_id',
+          'composer', 'milliseconds', 'bytes', 'unit_price']
+
+genres = ['genre_id', 'name']
+
+media_types = ['media_type_id', 'name']
+
+playlists = ['playlist_id', 'name', 'user_id']
+
+playlist_tracks = ['playlist_id', 'track_id', 'playlist_track_id']
 
 
 def main():
@@ -26,27 +60,32 @@ def main():
     if choice == "Home":
         st.subheader("HomePage")
 
-        #Columns/Layout
-        col1,col2 = st.columns(2)
+        # Columns/Layout
+        col1, col2 = st.columns(2)
 
         with col1:
             with st.form(key='query_form'):
                 raw_code = st.text_area("SQL Code Here")
                 submit_code = st.form_submit_button("Execute")
 
-            #TABLE of INFORMATION
-
-            with st.expander("Table Info"):
-                table_info = {'city':city,'country':country,'country_language':countrylanguage}
+            # TABLE of INFORMATION
+            with st.expander("Country Information"):
+                table_info = {'city': city, 'country': country, 'country_language': countrylanguage}
                 st.json(table_info)
 
-        #RESULTS LAYOUTS
+            with st.expander("Employee Information"):
+                table_info = {'employees': employees, 'customers': customers, 'invoices': invoices,
+                              'invoice_items': invoice_items, 'tracks': tracks, 'genres': genres,
+                              'media_types': media_types, 'playlists': playlists, 'playlist_tracks': playlist_tracks}
+                st.json(table_info)
+
+        # RESULTS LAYOUTS
         with col2:
             if submit_code:
                 st.info("Query Submitted")
                 st.code(raw_code)
 
-                #RESULTS
+                # RESULTS
                 query_results = sql_executor(raw_code)
                 with st.expander("Results"):
                     st.write(query_results)
@@ -57,8 +96,6 @@ def main():
 
     else:
         st.subheader("About")
-
-
 
 
 if __name__ == "__main__":
